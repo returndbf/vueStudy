10d16c8f6e0b0b2ec559e8a60241dbcdb7906dfa
returndbf <1668959325@qq.com>
2021-12-20 21:57:42 +0800

    详情页继续开发

---
+++ src/views/detail/childComponents/DetailComment.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+  <div class="username">
+    {{comment.username}}
+  </div>
+  <div class="comment-info">
+    {{comment.comment}}
+  </div>
+  <div>
+  <span class="time">
+    {{comment.time}}
+  </span>
+  <span class="goods-info">
+    {{comment.goodsInfo}}
+  </span>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "DetailComment",
+  props:["comment"]
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/detail/childComponents/DetailRecommend.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  这是推荐信息
+</div>
+</template>
+
+<script>
+export default {
+  name: "DetailRecommend"
+}
+</script>
+
+<style scoped>
+
+</style>
